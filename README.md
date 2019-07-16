b2eca54ea19a93cf13e529c75dfc0c2b08e0b48d
M P Bouw <superchiel94@hotmail.com>
2019-07-16 21:14:35 +0200

    init application first part

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Basket
+
 Online grocery shopping list management
+
+## Content
+
+Main view: `index.html`.
+
+Additional folders (needed for fully function web page): `assets`, `css` and `js`.
+
+## Colors
+
+```
+Background: #F9F8EB
+Boxes: #A7D2CB
+Highlights: #C98474
+Emphasis: #874C62
+```
---
+++ js/main.js
@@ -0,0 +1,55 @@
+function openMenu() {
+  var openIcon = document.getElementById('menu-open-icon');
+  openIcon.style.display = 'none';
+  openIcon.tabIndex = '-1';
+  var closeIcon = document.getElementById('menu-close-icon');
+  closeIcon.style.display = 'inline';
+  closeIcon.tabIndex = '0';
+
+  var template =
+    '<div class="menu-overlay">' +
+      '<div class="container menu-content">' +
+        '<h2>Menu</h2>' +
+        '<ul>' +
+          '<li tabindex="0"><a>My Baskets</a></li>' +
+        '</ul>' +
+        '<ul class="menu-bottom">' +
+          '<li tabindex="0"><a aria-label="View your profile">&#128100;&nbsp;&nbsp;<b>Sam</b></a></li>' +
+          '<li tabindex="0"><a aria-label="Setup your Basket and share with friends">Settings</a></li>' +
+          '<li tabindex="0"><a aria-label="More information about Basket">Help</a></li>' +
+          '<li tabindex="0"><a>Sign out</a></li>' +
+        '</ul>' +
+      '</div>' +
+      '<footer>' +
+        '<div class="container">' +
+          '<div class="footer-content">' +
+            '<img src="./assets/logo.png" />' +
+            'Basket | 2019 &copy; Michiel Bouw' +
+          '</div>' +
+        '</div>' +
+      '</footer>' +
+    '</div>';
+  var overlayWrapper = document.getElementById('overlay-wrapper');
+  overlayWrapper.innerHTML = template;
+
+  // focus on first nav item
+  document.querySelector('.menu-content li').focus();
+
+  var body = document.querySelector('body');
+  body.style.overflow = 'hidden';
+}
+
+function closeMenu() {
+  var openIcon = document.getElementById('menu-open-icon');
+  openIcon.style.display = 'inline';
+  openIcon.tabIndex = '0';
+  var closeIcon = document.getElementById('menu-close-icon');
+  closeIcon.style.display = 'none';
+  closeIcon.tabIndex = '-1';
+
+  var overlayWrapper = document.getElementById('overlay-wrapper');
+  overlayWrapper.innerHTML = '';
+
+  var body = document.querySelector('body');
+  body.style.overflow = '';
+}
